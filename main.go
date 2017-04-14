7b3b2b64ded38fdad00cf9994b07a8a895d48439
woremacx <woremacx@gmail.com>
2017-04-14 19:50:33 +0900

    customize

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
@@ -10,41 +12,55 @@ import (
 	"path"
 	"strconv"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 var portNumber = flag.String("port", "8080", "port number.")
-var basicAuthUser = flag.String("user", "", "basic auth user name")
-var basicAuthPass = flag.String("pass", "", "basic auth user pass")
-var urlPath = flag.String("path", "gyazo", "path for image url")
+
+var dir string
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	// pp.Print(r)
-
 	fmt.Fprintf(w, "hello "+r.URL.Path)
 }
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	// pp.Print(r)
+func makeBasename() string {
+	basename := strconv.FormatInt(time.Now().UnixNano(), 10) + os.Getenv("SECRET")
 
-	dir, err := os.Getwd()
-	if err != nil {
+	h := sha1.New()
+	h.Write([]byte(basename))
+	bs := h.Sum(nil)
+
+	dst := make([]byte, hex.EncodedLen(len(bs)))
+	hex.Encode(dst, bs)
+
+	return string(dst)
+}
+
+func genDirname(s string) string {
+	dst := path.Join("images", string(s[0]), string(s[0:2]))
+	return dst
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	basepart := makeBasename()
+	imagedir := genDirname(basepart)
+
+	imagedirpath := path.Join(dir, imagedir)
+	if err := os.MkdirAll(imagedirpath, 0755); err != nil && !os.IsExist(err) {
 		fmt.Fprintln(w, err)
 		return
 	}
-	imagedir := path.Join(dir, *urlPath, "images")
-	if err := os.MkdirAll(imagedir, 0755); err != nil && !os.IsExist(err) {
-		fmt.Fprintln(w, err)
-		return
-	}
+
 	file, _, err := r.FormFile("imagedata")
 	if err != nil {
 		fmt.Fprintln(w, err)
 		return
 	}
-
 	defer file.Close()
-	basename := strconv.FormatInt(time.Now().UnixNano(), 10) + ".png"
-	imagefile := path.Join(imagedir, basename)
+
+	basename := basepart + ".png"
+	imagefile := path.Join(imagedirpath, basename)
 	out, err := os.Create(imagefile)
 	if err != nil {
 		fmt.Fprintln(w, err)
@@ -58,54 +74,42 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// pp.Print(header)
-	fmt.Fprintf(w, "https://%s/%s/images/%s", r.Host, *urlPath, basename)
-}
+	url := fmt.Sprintf("%s/images/%s", os.Getenv("URLBASE"), basename)
+	fmt.Println(url)
 
-func checkAuth(w http.ResponseWriter, r *http.Request) bool {
-	if *basicAuthUser == "" || *basicAuthPass == "" {
-		return true
-	}
-
-	username, password, ok := r.BasicAuth()
-	// log.Println(username, password, ok)
-	if ok == false {
-		return false
-	}
-	return username == *basicAuthUser && password == *basicAuthPass
+	fmt.Fprintf(w, url)
 }
 
 func imagesHandler(w http.ResponseWriter, r *http.Request) {
-	// pp.Print(r)
-
-	if checkAuth(w, r) == false {
-		w.Header().Set("WWW-Authenticate", `Basic realm="Atto"`)
-		w.WriteHeader(401)
-		w.Write([]byte("401 Unauthorized\n"))
-		return
-	}
-
 	dir, err := os.Getwd()
 	if err != nil {
 		fmt.Fprintln(w, err)
 		return
 	}
-	// pp.Print(r)
+
 	imagefile := path.Join(dir, r.URL.Path)
-	// pp.Print(imagefile)
+
 	http.ServeFile(w, r, imagefile)
 }
 
 func main() {
-	flag.Parse()
-	if *basicAuthUser != "" && *basicAuthPass != "" {
-		log.Println("basic auth: " + *basicAuthUser)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Error cannot get Getwd")
+		return
+	}
+	dir = pwd
+
+	flag.Parse()
 	log.Println("listen:" + *portNumber)
-	log.Println("path:" + *urlPath)
 
 	http.HandleFunc("/", rootHandler)
-	http.HandleFunc(fmt.Sprintf("/%s/images/", *urlPath), imagesHandler)
-	http.HandleFunc(fmt.Sprintf("/%s/upload", *urlPath), uploadHandler)
+	http.HandleFunc("/images/", imagesHandler)
+	http.HandleFunc("/upload.cgi", uploadHandler)
 	http.ListenAndServe(":"+*portNumber, nil)
 }
